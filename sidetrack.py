601eabde23b35ded1b78784d88eebf8355ee7e27
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2020-06-14 00:30:37 +0200

    Return more register fields

--- sidetrack.py
+++ sidetrack.py
@@ -15,7 +15,18 @@ class InstructionFormatter:
     def format_arg(self, op: X86Op):
         labels = {
             X86_REG_AX: 'ax',
+            X86_REG_BP: 'bp',
+            X86_REG_BX: 'bx',
+            X86_REG_CX: 'cx',
+            X86_REG_DX: 'dx',
+            X86_REG_SI: 'si',
+            X86_REG_EAX: 'eax',
+            X86_REG_EBP: 'ebp',
+            X86_REG_EBX: 'ebx',
+            X86_REG_ECX: 'ecx',
+            X86_REG_EDI: 'edi',
             X86_REG_EDX: 'edx',
+            X86_REG_ESI: 'esi',
         }
 
         label = labels.get(op.reg, None)
